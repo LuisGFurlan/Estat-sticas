d38257cd3a31f41ed80696240c7c59d2bde4db1e
LuisGFurlan <furlanluisgustavo03@gmail.com>
2025-05-27 16:15:28 -0300

    fix : overflow | card Ativos

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -85,7 +85,7 @@ class _HomePageState extends State<HomePage> {
                     child: CategoriasCustom(),
                   )),
                   Expanded(child: Padding(
-                    padding: const EdgeInsets.all(10.0),
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
                     child: AtivosCustom(),
                   )),
                 ],
--- lib/widgets/charts_custom.dart
+++ lib/widgets/charts_custom.dart
@@ -12,91 +12,88 @@ class ChartCustom extends StatefulWidget {
 class _ChartCustomState extends State<ChartCustom> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20),
-      child: Container(
-        width: 0.3.sw,
-        child: Column(
-          children: [
-            Container(
-              height: 125,
-              //color: Colors.amber,
-              child: BarChart(
-                BarChartData(
-                  gridData: FlGridData(show: false),
-                  groupsSpace: 10,
-                  titlesData: FlTitlesData(
-                    show: false,
-                  ), // remove labels dos eixos
-                  borderData: FlBorderData(show: false), // remove bordas
-                  alignment: BarChartAlignment.center,
-                  barGroups: [
-                    BarChartGroupData(
-                      x: 0,
-                      barRods: [
-                        BarChartRodData(
-                          toY: 3,
-                          width: 30,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(5),
-                            topRight: Radius.circular(5),
-                          ),
-                          color: Color(0xFF253B59),
-                        ), //tamanho da barra
-                      ],
-                    ),
-                    BarChartGroupData(
-                      x: 0,
-                      barRods: [
-                        BarChartRodData(
-                          toY: 4,
-                          width: 30,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(5),
-                            topRight: Radius.circular(5),
-                          ),
-                          color: Color(0xFF5E9FF2),
-                        ), //tamanho da barra
-                      ],
-                    ),
-                    BarChartGroupData(
-                      x: 0,
-                      barRods: [
-                        BarChartRodData(
-                          toY: 6,
-                          width: 30,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(5),
-                            topRight: Radius.circular(5),
-                          ),
-                          color: Color(0xFF5DBCDF),
-                        ), //tamanho da barra
-                      ],
-                    ),
-                    BarChartGroupData(
-                      x: 0,
-                      barRods: [
-                        BarChartRodData(
-                          toY: 7,
-                          width: 30,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(5),
-                            topRight: Radius.circular(5),
-                          ),
-                          color: Color(0xFF3060BF),
-                        ), //tamanho da barra
-                      ],
-                    ),
-                  ],
-                ),
+    return Container(
+      width: 0.3.sw,
+      child: Column(
+        children: [
+          Container(
+            height: 125,
+            //color: Colors.amber,
+            child: BarChart(
+              BarChartData(
+                gridData: FlGridData(show: false),
+                groupsSpace: 10,
+                titlesData: FlTitlesData(
+                  show: false,
+                ), // remove labels dos eixos
+                borderData: FlBorderData(show: false), // remove bordas
+                alignment: BarChartAlignment.center,
+                barGroups: [
+                  BarChartGroupData(
+                    x: 0,
+                    barRods: [
+                      BarChartRodData(
+                        toY: 3,
+                        width: 20.w,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(5),
+                          topRight: Radius.circular(5),
+                        ),
+                        color: Color(0xFF253B59),
+                      ), //tamanho da barra
+                    ],
+                  ),
+                  BarChartGroupData(
+                    x: 0,
+                    barRods: [
+                      BarChartRodData(
+                        toY: 4,
+                        width: 30,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(5),
+                          topRight: Radius.circular(5),
+                        ),
+                        color: Color(0xFF5E9FF2),
+                      ), //tamanho da barra
+                    ],
+                  ),
+                  BarChartGroupData(
+                    x: 0,
+                    barRods: [
+                      BarChartRodData(
+                        toY: 6,
+                        width: 30,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(5),
+                          topRight: Radius.circular(5),
+                        ),
+                        color: Color(0xFF5DBCDF),
+                      ), //tamanho da barra
+                    ],
+                  ),
+                  BarChartGroupData(
+                    x: 0,
+                    barRods: [
+                      BarChartRodData(
+                        toY: 7,
+                        width: 30,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(5),
+                          topRight: Radius.circular(5),
+                        ),
+                        color: Color(0xFF3060BF),
+                      ), //tamanho da barra
+                    ],
+                  ),
+                ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 5),
-              child: Container(color: Colors.black,  height: 5),
-            ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 5),
+            child: Container(color: Colors.black,  height: 5),
+          ),
+        ],
       ),
     );
   }
